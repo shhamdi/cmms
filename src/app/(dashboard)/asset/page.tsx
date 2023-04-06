865012257f851709d1ad2a8e7f10045f5cbaf7c6
Anis-Moulahi <moulahianis2000@gmail.com>
2023-04-06 13:59:53 +0100

    update asset

--- src/app/(dashboard)/asset/page.tsx
+++ src/app/(dashboard)/asset/page.tsx
@@ -1,7 +1,45 @@
-export default function Asset() {
+"use client";
+import React from "react";
+import Forms from "./Forms";
+import { Row, Col, Form } from "react-bootstrap";
+import Button from "react-bootstrap/Button";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { useState } from "react";
+import "/home/anis/Desktop/PFA/cmms/src/app/(dashboard)/asset/style.css";
+const Assets = () => {
+  const [isClicked, setisClicked] = useState(false);
+  function add() {
+    setisClicked(!isClicked);
+  }
+  function cancel() {
+    setisClicked(!isClicked);
+  }
   return (
     <div>
-      <div>Asset </div>
+      {isClicked ? (
+        <div>
+          <Button className="addButton" onClick={cancel}>
+            Cancel
+          </Button>
+          <Forms />
+        </div>
+      ) : (
+        <div>
+          <nav className="navbar">
+            <h1>Assets</h1>
+
+            <Form.Group className="search">
+              <Form.Control type="search" placeholder="search" />
+              <Button>Search</Button>
+            </Form.Group>
+            <Button onClick={add} className="add">
+              Add Asset
+            </Button>
+          </nav>
+        </div>
+      )}
     </div>
   );
-}
+};
+
+export default Assets;
